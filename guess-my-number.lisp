19b6a2cb3c769839586c666d0cd014451a3cdd85
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2012-11-24 12:52:43 -0800

    First commit

---
+++ guess-my-number.lisp
@@ -0,0 +1,23 @@
+;;
+;; The "guess my number" game from Chapter 1
+;;
+
+;; limits
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over ()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
+  (guess-my-number))
